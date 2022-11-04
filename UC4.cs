c84083a4f4c54ba3427211255935e9110546ec28
AnilBamne <99521630+AnilBamne@users.noreply.github.com>
2022-11-04 20:54:04 +0530

    UC4_FindTheAverageAgeOfPersons

---
+++ UC4.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UC4
+{
+    class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+        public Person(string ssn, string name, string addr, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = addr;
+            Age = age;
+        }
+    }
+    class UC4
+    {
+        public static void AddRecords(List<Person> listPersonInCity)
+        {
+            listPersonInCity.Add(new Person("203456876", "John", "12 Main Street, Newyork,NY", 15));
+            listPersonInCity.Add(new Person("203456877", "SAM", "13 Main Ct, Newyork,NY", 17));
+            listPersonInCity.Add(new Person("203456878", "Elan", "14 Main Street, Newyork,NY", 35));
+            listPersonInCity.Add(new Person("203456879", "Smith", "12 Main Street, Newyork,NY", 45));
+            listPersonInCity.Add(new Person("203456880", "SAM", "345 Main Ave, Dayton,OH", 55));
+            listPersonInCity.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork,NY", 65));
+            listPersonInCity.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65));
+            listPersonInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore,NY", 85));
+            listPersonInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore,NY", 95));
+            // Console.WriteLine(listPersonInCity.ToString());
+            // listPersonInCity.ForEach(x => Console.WriteLine("{0}\t",x.Name.ToString()));
+
+        }
+
+        public static void AllPersonsAverageAge(List<Person> listPersonInCity)
+        {
+            double avgAge = listPersonInCity.Average(e => e.Age);
+            Console.WriteLine("The average of all the person's age is :" + avgAge);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> listPersonInCity = new List<Person>();
+            UC4.AddRecords(listPersonInCity);
+            UC4.AllPersonsAverageAge(listPersonInCity);
+        }
+    }
+}
